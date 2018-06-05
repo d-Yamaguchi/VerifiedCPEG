849d10f7b12b1183fff9a5d56027d2d255899b6c
Daisuke YAMAGUCHI <yamagc@greenApple.local>
2018-06-06 01:29:42 +0900

    undertaking issue2

--- VeriCPEG.thy
+++ VeriCPEG.thy
@@ -1,5 +1,5 @@
 theory VeriCPEG
-imports Main
+imports Main "~~/src/HOL/Nominal/Nominal"
 
 begin
 
@@ -28,11 +28,21 @@ and  nodeExp =
 datatype cpegKind = treekind | stringkind
 
 (* Kinding Rule is defind as follows *)
+inductive kined ("_ \<turnstile> _ :: _" 40) where
+  kEmpty: "[] \<turnstile> empty :: stringkind"
+| kString: "[] \<turnstile> terminal _ :: stringkind"
+| kNt : "\<lbrakk>((nonterm e),\<kappa>)#\<Delta> \<turnstile> e :: \<kappa>\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (nonterm e) :: \<kappa>"
+| kSeq : "\<lbrakk>\<Delta> \<turnstile> e1:: stringkind; \<Delta> \<turnstile> e2 :: stringkind\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (seq e1 e2) :: stringkind"
+| kAlt :  "\<lbrakk>\<Delta> \<turnstile> e1 :: \<kappa>; \<Delta> \<turnstile> e2 :: \<kappa>\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (seq e1 e2) :: \<kappa>"
+| kRep : "\<lbrakk>\<Delta> \<turnstile> e :: stringkind\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (rep e) :: stringkind"
+| kNot : "\<lbrakk>\<Delta> \<turnstile> e :: stringkind\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (not e) :: stringkind"
+| kLeaf : "\<lbrakk>\<Delta> \<turnstile> e :: stringkind\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (leaf _ e) :: treekind"
+(* | kCapture : "\<lbrakk>\<forall> \<xi> \<in> l. \<Delta> \<turnstile> \<xi> :: treekind\<rbrakk> \<Longrightarrow> \<Delta> \<turnstile> (capture _ l) :: treekind"*)
 
 (* but not yet *)
-
+                
 section{* Parsing Semantics *}
-(* parsing results *)
+(** parsing results *)
 
 (* tree is a final value of parsing result of well-formed expressions*)
 datatype tree =
@@ -41,12 +51,15 @@ datatype tree =
 
 (* outputP includes final value and intermediate*)
 datatype outputP = 
-  fail
+  fail                                                 
   | simbols string
   | asts tree
-  | none 
-  
+  | none
 
-
+(* parsing function*)
+fun parsing :: "cpegExp \<Rightarrow> string \<Rightarrow> (outputP \<times> string)" where
+  "parsing empty xs = (simbols [],xs)"
+| "parsing (terminal a) (x#xs) = (if a = x then (simbols ''a'',xs) else (fail, (x#xs)))"
+| "parsing (nonterm e) xs = (case parsing e xs of )"
 
 end
