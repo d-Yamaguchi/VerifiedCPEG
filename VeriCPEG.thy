3c8727229e7f921a935aed01d5631e9a203be80f
Daisuke YAMAGUCHI <yamagc@greenApple.local>
2018-06-06 18:43:46 +0900

    defined parsing rule for a capture expression and evalCapturedSubterm for it

--- VeriCPEG.thy
+++ VeriCPEG.thy
@@ -46,20 +46,71 @@ section{* Parsing Semantics *}
 
 (* tree is a final value of parsing result of well-formed expressions*)
 datatype tree =
-  baseVal baseTypes
+  baseValString string
+  | baseValInt int
+  | baseValBool bool
   | nodeVal string "(string \<times> tree) list"  
 
 (* outputP includes final value and intermediate*)
 datatype outputP = 
   fail                                                 
   | simbols string
-  | asts tree
-  | none
+  | ast tree
+(*| none   *)
 
 (* parsing function*)
+(*
+Each input cpegExp for paring must be a well-formed expression.
+How to describe this conditioning in the type of parsing?
+*)
+
 fun parsing :: "cpegExp \<Rightarrow> string \<Rightarrow> (outputP \<times> string)" where
   "parsing empty xs = (simbols [],xs)"
 | "parsing (terminal a) (x#xs) = (if a = x then (simbols ''a'',xs) else (fail, (x#xs)))"
-| "parsing (nonterm e) xs = (case parsing e xs of )"
-
+| "parsing (nonterm e) xs =  parsing e xs"
+| "parsing (seq e1 e2) xs = (case parsing e1 xs of
+                             (fail,_) \<Rightarrow> (fail,xs)
+                           | (simbols x,ys) \<Rightarrow> (case parsing e2 ys of
+                                                (fail,_) \<Rightarrow> (fail,xs)
+                                              | (simbols y,zs) \<Rightarrow> (simbols (x@y),zs)
+                                              )
+                           (* if e1 and e2 are well-formed expressions, they will not derive a value ast (TODO: SHOW IT!) *)
+                          )"
+| "parsing (alt e1 e2) xs = (case parsing e1 xs of
+                             (fail,_) \<Rightarrow> parsing e2 xs
+                           | (simbols x, ys) \<Rightarrow> (simbols x, ys)
+                           | (ast t, ys) \<Rightarrow> (ast t, ys)
+                            )"
+| "parsing (not e) xs = (case parsing e xs of
+                          (fail,_) \<Rightarrow> (simbols [],xs)
+                        | (_, _) \<Rightarrow> (fail, xs)
+                        )"
+| "parsing (leaf ty e) xs = (case parsing e xs of
+                          (fail, _) \<Rightarrow> (fail, xs)
+                        | (simbols x, ys) \<Rightarrow> (case ty of
+                                               bString \<Rightarrow> (ast (baseValString x), ys)
+                                             | bInt    \<Rightarrow> (ast (baseValInt 1), ys)
+                                             | bBool   \<Rightarrow> (ast (baseValBool true), ys)
+                                             )
+                        )"
+                        (* if e is a well-formed expression it will not derive a value ast (TODO: SHOW IT!) *)
+ | "parsing (capture l xis) xs = (case evalCapturedSubterm xis xs of
+                                  None \<Rightarrow> (fail, xs)
+                                | Some (subtrees, ys) \<Rightarrow> (ast (nodeVal l subtrees),ys)
+                                )"
+and evalCapturedSubterm :: "(nodeExp list) \<Rightarrow> string \<Rightarrow> ((string\<times>tree) list \<times> string) option" where
+  "evalCapturedSubterm [] xs = Some ([], xs)"
+| "evalCapturedSubterm (e#es) xs = (case e of
+                                     subtree \<nu> eSub \<Rightarrow> (case parsing eSub xs of
+                                                          (fail, _) \<Rightarrow> None
+                                                        | (ast d, ys) \<Rightarrow> (case evalCapturedSubterm es ys of
+                                                                            None \<Rightarrow> None
+                                                                          | Some (dyss,zs) \<Rightarrow> Some ((\<nu>, d)#dyss,zs)
+                                                                         )
+                                                       )
+                                   | absorb eSub \<Rightarrow> (case parsing eSub xs of
+                                                       (fail,_) \<Rightarrow> None
+                                                     | (simbols x, ys) \<Rightarrow> Some ([],ys)
+                                                    )
+                                   )"
 end
